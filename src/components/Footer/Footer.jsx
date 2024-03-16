7169d933ed1f80d838512096b0dfd69df2c39601
shashwat288 <shashwatsaroj312@gmail.com>
2024-03-16 18:23:06 +0530

    Shashwat's Website
    Shashwat's Website

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Footer.css";
+import Wave from "../../img/wave.png";
+import Insta from "@iconscout/react-unicons/icons/uil-instagram";
+import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
+import Gitub from "@iconscout/react-unicons/icons/uil-github";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <img src={Wave} alt="" style={{ width: "100%" }} />
+      <div className="f-content">
+        <span>shashwatsaroj312@gmail.com</span>
+        <div className="f-icons">
+          <a href="https://www.instagram.com/shashwat_288" target="_blank">
+            <Insta color="white" size={"3rem"} />
+          </a>
+
+          <a href="https://www.facebook.com/" target="_blank">
+            <Facebook color="white" size={"3rem"} />
+          </a>
+
+          <a href="https://github.com/shashwat288" target="_blank">
+            <Gitub color="white" size={"3rem"} />
+          </a>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,53 @@
+
+import React, { useContext } from "react";
+import "./Portfolio.css";
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css";
+import Sidebar from "../../img/sidebar.png";
+import Ecommerce from "../../img/ecommerce.png";
+import img1 from "../../img/img1.png";
+import img2 from "../../img/img2.png";
+import img33 from "../../img/img33.png";
+import img4 from "../../img/img4.png";
+import img5 from "../../img/img5.png";
+
+import HOC from "../../img/hoc.png";
+import MusicApp from "../../img/musicapp.png";
+import { themeContext } from "../../Context";
+const Portfolio = () => {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <div className="portfolio" id="portfolio">
+      {/* heading */}
+      <span style={{color: darkMode?'white': ''}}>Recent Projects</span>
+      <span>Portfolio</span>
+
+      {/* slider */}
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={3}
+        grabCursor={true}
+        className="portfolio-slider"
+      >
+        <SwiperSlide>
+          <img src={img2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img33} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img4} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img5} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img1} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Portfolio;
